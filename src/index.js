8736f16a06af909db26f66d7cf413e25a3922b4a
Mayonk Kumar Behera <mayonk.kumar9@gmail.com>
2022-07-09 19:17:01 +0530

    trying out hashrouter to fix the on refresh no page found error

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import ScrollToTopOnRoute from "./components/ScrollToTopOnRoute";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ScrollToTopOnRoute />
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
